7e2ffb035d951802b9ff37faf9ecd476023c0f1e
krummy0 <jlkrumholz01@gmail.com>
2024-10-31 12:39:00 -0600

    Update Start.java

--- src/Start.java
+++ src/Start.java
@@ -1,32 +1,28 @@
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 public class Start {
-	private static String input;
+	private static List<String> input = new ArrayList<String>();
 	private static String api;
 	private static String cx;
-	private static int count;
+	private static int count = 100;
 	private static String include;
 	private static String exclude;
 	private static String country;
 	private static String savePath = "Output/output.txt";
 	private static String userAgentsPath;
 	private static int maxThreads = 5;
-	private static String proxyFile = "Resources/proxies.txt";
 	
-    public static void main(String[] args) {    	
-    	//allow basic auth for proxies
-		Properties props = System.getProperties();
-		props.setProperty("jdk.http.auth.tunneling.disabledSchemes", "");
-    	
+    public static void main(String[] args) {
         Map<String, String> flagMap = new HashMap<>();
 
         // Check if any arguments were passed
-        if (args.length == 0) {
+        if (args.length == 0 || args[0].equals("-h")) {
             help();
             return;
         }
@@ -49,8 +45,8 @@ public class Start {
 
         for (Map.Entry<String, String> entry : flagMap.entrySet()) {
         	//required
-            if (entry.getKey().equals("-s") || entry.getKey().equals("--search")) {
-            	input = entry.getValue();
+            if (entry.getKey().startsWith("-s") || entry.getKey().startsWith("--search")) {
+            	input.add(entry.getValue());
             }
             else if (entry.getKey().equals("-c") || entry.getKey().equals("--count")) {
             	count = Integer.parseInt(entry.getValue());
@@ -71,9 +67,6 @@ public class Start {
             else if (entry.getKey().equals("--threads")) {
             	maxThreads = Integer.parseInt(entry.getValue());
             }
-            else if (entry.getKey().equals("--proxy-file")) {
-            	proxyFile = entry.getValue();
-            }
             else if (entry.getKey().equals("--country")) {
             	country = entry.getValue();
             }
@@ -87,9 +80,7 @@ public class Start {
         
         if (userAgentsPath != null)
         	Scrapper.setJSONFile(userAgentsPath);
-        Scrapper.setProxyFile(proxyFile);
         Scrapper.setAttempts(1);
-        Scrapper.setDelimiter(':');
         ShopifyScrapper.setThreads(maxThreads);
 		ShopifyScrapper.setGoogleApi(api);
 		ShopifyScrapper.setCx(cx);
@@ -104,7 +95,11 @@ public class Start {
     	try {
     		System.out.println("Starting");    		
     		//scrape
-    		ShopifyScrapper scrape = new ShopifyScrapper(input, include, exclude, country, count);
+    		List<ShopifyScrapper> scraperList = new ArrayList<>();
+    		for (String str : input) {
+    			System.out.println("Searching: " + str);
+    			scraperList.add(new ShopifyScrapper(str, include, exclude, country, count));
+    		}
     		
     		System.out.println("Completed! Saving Data");
     		
@@ -118,24 +113,35 @@ public class Start {
             }
 
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-                for (String item : scrape.getShops()) {
-                    writer.write(item);
-                    writer.newLine(); // Write each item on a new line
+                for (ShopifyScrapper scrape : scraperList) {
+                	writer.write(scrape.getSearch() + ":");
+                	writer.newLine();
+                	for (String item : scrape.getShops()) {
+                		writer.write(item);
+                		writer.newLine(); // Write each item on a new line
+                	}
                 }
+
             } catch (Exception e) {
                 System.err.println("Failed to save to file. Outputting to console. Error:");
                 e.printStackTrace();
                 System.out.println();
                 System.out.println();
                 System.out.println();
-                for (String str : scrape.getShops())
-                    System.out.println(str);
+                for (ShopifyScrapper scrape : scraperList) {
+                	System.out.println(scrape.getSearch() + ":");
+	                for (String str : scrape.getShops())
+	                    System.out.println(str);
+                }
             }
             
             //output stats
             System.out.println("Results:");
-            System.out.println("\tShops Found: " + scrape.getShops().size());
-            System.out.println("\t% of Hits: " + scrape.getPercent());
+            for (ShopifyScrapper scrape : scraperList) {
+            	System.out.println("\t" + scrape.getSearch() + ":");
+                System.out.println("\t\tShops Found: " + scrape.getShops().size());
+                System.out.println("\t\t% of Hits: " + scrape.getPercent());
+            }
     	}
     	catch (Exception e) {
     		e.printStackTrace();
@@ -147,12 +153,12 @@ public class Start {
     	
     	System.out.println("\tRequired Flags:");
     	System.out.println("\t\t-s or --search:     String to search for");
-    	System.out.println("\t\t			        (Put it in quotes if its more than one word)");
-    	System.out.println("\t\t-c or --count:      How many websites to check");
+    	System.out.println("\t\t			        (Put it in quotes if its more than one word. You can pass multiple searches by using -s1, -s2 etc)");
     	System.out.println("\t\t--api:              Google Search API Key");
     	System.out.println("\t\t--cx:               Google Search Browser CX key");
     	
     	System.out.println("\tOptional Flags:");
+    	System.out.println("\t\t-c or --count:      How many websites to check (Google API maxes at 100 per search) (Default is 100)");
     	System.out.println("\t\t--output-file:     Program will save information to this file in json format");
     	System.out.println("\t\t                   (Defualt is Output/output.txt)");
     	System.out.println("\t\t--useragents-file: Program will read list of user agents from this file");
@@ -160,12 +166,10 @@ public class Start {
     	System.out.println("\t\t                   (Download lists from: https://www.useragents.me/)");
     	System.out.println("\t\t--threads:         How many threads to use");
     	System.out.println("\t\t                   (Defualt is 5)");
-    	System.out.println("\t\t--proxy-file:      File to read proxies from");
-    	System.out.println("\t\t                   (Defualt is Resources/proxies.txt)");
     	System.out.println("\t\t--country:         Search in specific country");
     	System.out.println("\t\t                   (Only use valid strings. See link below)");
     	System.out.println("\t\t                   (https://developers.google.com/custom-search/docs/json_api_reference#countryCollections)");
-    	System.out.println("\t\t--exact:           Search for websites containing exacltly this string");;
-    	System.out.println("\t\t--exclude:         Search for websites that do not include this string");    	
+    	System.out.println("\t\t--exact:           Search for websites containing exacltly this string (applies to all searches)");
+    	System.out.println("\t\t--exclude:         Search for websites that do not include this string (applies to all searches)");    	
     }
 }
